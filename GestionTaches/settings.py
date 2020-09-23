d86b0a62c8d152cd86b48925b55bfa3914b06a0f
Gerard Rozsavolgyi <roza@univ-orleans.fr>
2020-09-23 15:11:17 +0200

    update project dependancies

--- GestionTaches/settings.py
+++ GestionTaches/settings.py
@@ -60,7 +60,8 @@ ROOT_URLCONF = 'GestionTaches.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
