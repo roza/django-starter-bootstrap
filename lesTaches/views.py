ae7dbedb1d955a17a58e78a0969113d5d989320a
Gerard Rozsavolgyi <roza@univ-orleans.fr>
2020-10-01 09:24:35 +0200

    added few tests

--- lesTaches/views.py
+++ lesTaches/views.py
@@ -6,6 +6,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+def accueil(request):
+    return HttpResponse("Bienvenue ici")
+
 def home(request, param):
     return HttpResponse("Hello Django ! "+param)
 
