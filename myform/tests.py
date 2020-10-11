7bdd4b565ee2282a90de592dc9e5a00e50a9ffc6
Gerard Rozsavolgyi <roza@univ-orleans.fr>
2020-10-11 19:26:21 +0200

    update tests selenium

--- myform/tests.py
+++ myform/tests.py
@@ -5,6 +5,8 @@ from selenium import webdriver
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 #from django.test import LiveServerTestCase
 from selenium.webdriver.chrome.options import Options
+from time import time
+import re
 
 class FunctionalTest(StaticLiveServerTestCase):
 
@@ -13,8 +15,8 @@ class FunctionalTest(StaticLiveServerTestCase):
         super().setUpClass()
         cls.options = Options()
         cls.options.add_argument("--headless")
-        cls.browser = webdriver.Chrome(executable_path='/usr/lib/chromium-browser/chromedriver',
-        options=cls.options)
+        #cls.browser = webdriver.Firefox()
+        cls.browser = webdriver.Chrome(executable_path='/usr/lib/chromium-browser/chromedriver',options=cls.options)
         cls.browser.implicitly_wait(3)
 
     @classmethod
@@ -22,10 +24,10 @@ class FunctionalTest(StaticLiveServerTestCase):
         cls.browser.quit()
         super().tearDownClass()
 
-    def test_can_show_task_list(self):
-        # Alice se rend sur le site localhost:8000
+    def test_can_show_creation_form(self):
+        # Alice se rend sur le site localhost:8000/contacts/
         # et compte y trouver une app de gestion des contacts
-        # sur la route /contacts/
+        # avec un formulaire de création 
         self.browser.get('http://127.0.0.1:8000/contacts/')
 
         # Elle remarque que le mot "Contact"
@@ -33,26 +35,25 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.assertIn('Contact', self.browser.title)
         self.fail('Test terminé !')
 
-        # Elle peut aussi créer une nouvelle tâche ...
-        # Puis constater qu'elle figure bien
-        # dans la page qui liste les tâches ...
+        # Elle peut aussi créer ce contact ...
+        # Puis constater qu'il figure bien
+        # dans la page qui liste les contacts ...
 
 
-    def test_add_task(self):
+    def test_can_add_and_del_contact(self):
         '''Test d'ajout et supression d'un contact '''
 
         # Alice se rend sur la route de création
-        # d'une nouvelle tâche
-        browser = webdriver.Chrome()
-        browser.get('http://localhost:8000/contacts/')
+        # d'un nouveau contact
+        self.browser.get('http://localhost:8000/contacts/')
         time.sleep(1)
         # Elle crée un nouveau contact appelé  "Al" "Ouette"
         # Ayant pour mail "al@ouette.org"
         # Avec le message "Lorem ipsum dolor sit amet."
-        name = browser.find_element_by_id("id_name")
-        firstname = browser.find_element_by_id("id_firstname")
-        message = browser.find_element_by_id("id_message")
-        email = browser.find_element_by_id("id_email")
+        name = self.browser.find_element_by_id("id_name")
+        firstname = self.browser.find_element_by_id("id_firstname")
+        message = self.browser.find_element_by_id("id_message")
+        email = self.browser.find_element_by_id("id_email")
         # Elle envoie son nom, prénom
         name.send_keys("Ouette")
         time.sleep(1)
@@ -62,39 +63,38 @@ class FunctionalTest(StaticLiveServerTestCase):
         email.send_keys("al@ouette.org")
         time.sleep(1)
         ## Elle crée un message
-        message.send_keys("Lorem ipsum dolor sit amet.")
+        message.send_keys("Lorem ipsum dolor sit amet ...")
         time.sleep(1)
 
         ## Alice valide le formulaire
-        browser.find_element_by_id("submit").click()
+        self.browser.find_element_by_id("submit").click()
         time.sleep(1)
 
-        #La tache doit à présent être créée si tout s'est bien passé
+        #Le contact doit à présent être créé si tout s'est bien passé
 
-        noms = browser.find_elements_by_css_selector("body > div > div > section.content-header > div > div > div > div.box-header > a > h4")
+        noms = self.browser.find_elements_by_css_selector("body > div > div > section.content-header > div > div > div > div.box-header > a > h4")
         time.sleep(1)
-        taskFound = False
+        contactFound = False
         elem = None
         for nom in noms:
-            if( re.match("Ma superbe tache", nom.text) ):
-                taskFound = True
+            if( re.match("Ouette", nom.text) ):
+                contactFound = True
                 elem = nom
 
-        #on vérifie si la tache est crée
-        self.assertEqual(True,taskFound)
+        #on vérifie si le contact a bien été trouvé
+        self.assertEqual(True,contactFound)
 
-        #à présent Alice va cliquer pour afficher la tâche
+        #à présent Alice va cliquer pour afficher les détails du contact
         elem.click()
         time.sleep(1)
-        #Puis sélectionner l'icone de suppression pour supprimer la tâche
-        browser.find_elements_by_css_selector("body > div > section.content-header > a:nth-child(1) > i")[0].click()
+        #Puis sélectionner l'icone de suppression pour supprimer le contact
+        self.browser.find_elements_by_css_selector("body > div > section.content-header > a:nth-child(1) > i")[0].click()
         time.sleep(1)
 
-        #on verifie que la tâche est supprimée
-        noms = browser.find_elements_by_css_selector("body > div > div > section.content-header > div > div > div > div.box-header > a > h4")
-        taskFound = False
+        #on verifie que le contact est bien supprimé
+        noms = self.browser.find_elements_by_css_selector("body > div > div > section.content-header > div > div > div > div.box-header > a > h4")
+        contactFound = False
         for nom in noms:
-            if( re.match("Ma super tache", nom.text) ):
-                taskFound = True
-
-        browser.quit()
+            if( re.match("Ouette", nom.text) ):
+                contactFound = True
+        self.assertEqual(False,contactFound)
