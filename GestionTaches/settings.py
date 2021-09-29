951c2c3906f6904567832be16f66e7f1611be32c
Gerard Rozsavolgyi <roza@univ-orleans.fr>
2021-09-29 08:54:15 +0200

    update settings.py

--- GestionTaches/settings.py
+++ GestionTaches/settings.py
@@ -41,10 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'lesTaches',
     'myform',
-    'crispy_forms',
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
-
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
